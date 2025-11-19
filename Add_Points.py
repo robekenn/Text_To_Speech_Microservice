c1d9cd7c8707518ce336f2edd65eb4a3fb845465
Kenneth Robertson <Kseth0626@gmail.com>
2025-11-19 12:32:33 -0800

    wrong MC

--- Add_Points.py
+++
@@ -1,6 +0,0 @@
-def add_points(userID, num, data):
-    for user in data.get("users", []):
-        if user.get("id") == userID:
-            user["points"] = user.get("points", 0) + num
-            return user.get("points", 0)
-    return "NA"
--- Get_Points.py
+++
@@ -1,5 +0,0 @@
-def get_points(userID, data):
-    for user in data.get("users", []):
-        if user.get("id") == userID:
-            return user.get("points", 0)
-    return "NA" 
